61b7e6bc3bd20bd8b444582452e22a3531311e51
azralmukmin <azral_m@yahoo.com>
2017-11-03 00:44:48 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Yup. That work for me.'
-    });
 });
 
 socket.on('disconnet', function () {
